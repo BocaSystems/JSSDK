4710de5b383ad7447ff8fb9269fe643a18ab092b
Boca Systems <bocasystems@hotmail.com>
2024-03-05 15:35:31 -0500

    22

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
+//Dependencies 
 const WebSocket = require('ws');
 const net = require('net');
 
-// Replace these with your printer's IP and port
+//Globals
 const PRINTER_IP = '10.0.2.126';
 const PRINTER_PORT = 9100;
 const PORT = process.env.PORT || 5050; 
+const INDEX = '/index.html';
 
 
-const wss = new WebSocket.Server({ port: PORT });
 
+
+//Working standalone server
+//const wss = new WebSocket.Server({ port: PORT });
+
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+//
+
+const { Server } = require('ws');
+const wss = new Server({ server });
+
+
+
+//test
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
+
+
+
+
+
+//server handlers
 wss.on('connection', function connection(ws) {
   console.log('A new client connected.');
   
